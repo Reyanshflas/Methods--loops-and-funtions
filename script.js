d8fb1b07c9dd1c83562f0958bb1134013007a1f4
Reyanshflas <reyanshflasplayz@gmail.com>
2024-12-14 10:12:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var str=("SounD_BOx")
+
+document.write(str.toUpperCase()) 
+
+document.write("<br>" + str.toLowerCase()) 
+
+
+let num = 13
+
+for(i=1 ; i<13 ; i++){
+
+    document.write(num + "*" + i + "=" + num*i + "<br>")
+}
